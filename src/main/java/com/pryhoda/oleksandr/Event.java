0aa3602344478e00952835ba3e0f46210a6410f1
Oleksandr Pryhoda <pryhoda@ucu.edu.ua>
2017-09-07 17:28:38 +0300

    Add files via upload

---
+++ src/main/java/com/pryhoda/oleksandr/Event.java
@@ -0,0 +1,40 @@
+package com.pryhoda.oleksandr;
+
+
+public class Event {
+
+    int eventId;
+    String eventName;
+    String eventDate;
+
+    public Event(String eventName, String eventDate, int eventId) {
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+}
