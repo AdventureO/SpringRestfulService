f542c7bea9b4b08bb9522aac9b42a0ec3f485916
Oles <pryhoda@ucu.edu.ua>
2017-09-07 12:17:13 +0300

    Second commit Can get Json object But still not working post method

--- src/main/java/com/pryhoda/oleksandr/Ticket.java
+++ src/main/java/com/pryhoda/oleksandr/Ticket.java
@@ -1,10 +1,9 @@
 package com.pryhoda.oleksandr;
 
 
-import org.json.simple.JSONObject;
-import java.io.FileWriter;
+import java.io.Serializable;
 
-public class Ticket {
+public class Ticket implements Serializable{
 
     int ticketId;
     String firstName;
@@ -31,31 +30,6 @@ public class Ticket {
 
     public String toString() {
         return this.firstName + " " + this.ticketId;
-    }
-
-    public void writeInFile() {
-        try {
-            JSONObject jsonObject = new JSONObject();
-
-            System.out.println(this.toString());
-            jsonObject.put("Id", this.ticketId);
-            jsonObject.put("FirstName", this.firstName);
-
-            FileWriter fileWriter = new FileWriter("test.json");
-            fileWriter.write(jsonObject.toJSONString());
-            fileWriter.close();
-
-            System.out.println("JSON Object Successfully written to the file!!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        Ticket tick = new Ticket(1, "lol" + 1);
-        System.out.println(tick.toString());
-        tick.writeInFile();
     }
 
 }
--- src/main/java/com/pryhoda/oleksandr/TicketService.java
+++ src/main/java/com/pryhoda/oleksandr/TicketService.java
@@ -1,28 +1,25 @@
 package com.pryhoda.oleksandr;
 
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.io.FileWriter;
+import java.util.*;
 
 @Controller
 @RequestMapping("/ticket")
 public class TicketService {
 
-    static Set Tickets;
+/*    static Set Tickets;
 
     static {
         Tickets = new HashSet();
         Ticket tick;
         for (int i = 0; i < 10; i++) {
             tick = new Ticket(i, "Ticket" + i);
-            tick.writeInFile();
+            //writeInFile(tick);
             Tickets.add(tick);
         }
     }
@@ -30,15 +27,48 @@ public class TicketService {
     @RequestMapping(value = "/{ticketId}", method = RequestMethod.GET, headers = "Accept=text/html", produces = {"text/html "})
     @ResponseBody
     public String getTicket(@PathVariable int ticketId) {
-        Iterator X = Tickets.iterator();
-        while (X.hasNext()) {
-            Ticket f = (Ticket) X.next();
+        Iterator iter = Tickets.iterator();
+        while (iter.hasNext()) {
+            Ticket f = (Ticket) iter.next();
             if (f.getTicketId() == ticketId) return f.toString();
         }
         return null;
+    }*/
+
+    Map<Integer, Ticket> empData = new HashMap<Integer, Ticket>();
+
+    @RequestMapping(value = "/dummy", method = RequestMethod.GET)
+    public @ResponseBody Ticket getDummyTicket() {
+        Ticket emp = new Ticket(0, "Dummy");
+        Ticket emp1 = new Ticket(1, "Vasyl");
+
+        empData.put(emp.ticketId, emp);
+        empData.put(emp1.ticketId, emp1);
+        return emp;
     }
 
-    @RequestMapping(method = RequestMethod.GET, headers = "Accept=text/html", produces = {"text/html"})
+    @RequestMapping(value = "/{ticketId}", method = RequestMethod.GET)
+    public @ResponseBody Ticket getTicket(@PathVariable("ticketId") int empId) {
+        return empData.get(empId);
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public @ResponseBody Ticket createTicket(@RequestBody Ticket emp) {
+        empData.put(emp.ticketId, emp);
+        return emp;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody List<Ticket> getAllTickets() {
+        List<Ticket> emps = new ArrayList<Ticket>();
+        Set<Integer> empIdKeys = empData.keySet();
+        for(Integer i : empIdKeys){
+            emps.add(empData.get(i));
+        }
+        return emps;
+    }
+
+/*    @RequestMapping(method = RequestMethod.GET, headers = "Accept=text/html", produces = {"text/html"})
     @ResponseBody
     public String getTickets() {
         Iterator X = Tickets.iterator();
@@ -49,6 +79,25 @@ public class TicketService {
         }
         return result;
     }
+
+    public static void writeInFile(Ticket ticket) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+
+            System.out.println(ticket.toString());
+            jsonObject.put("Id", ticket.ticketId);
+            jsonObject.put("FirstName", ticket.firstName);
+
+            FileWriter fileWriter = new FileWriter("test.json");
+            fileWriter.write(jsonObject.toJSONString());
+            fileWriter.close();
+
+            System.out.println("JSON Object Successfully written to the file!!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
 
 /*    @RequestMapping(value = "/list", method = RequestMethod.GET, headers = "Accept=application/json", produces = {"application/json"})
     @ResponseBody
